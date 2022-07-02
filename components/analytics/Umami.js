fbea2ec792359cce3d909e2c4c86d1627b9c07aa
Monica Miller <monicadd4@gmail.com>
2022-07-02 15:23:00 -0500

    Update Umami link to netlify

--- components/analytics/Umami.js
+++ components/analytics/Umami.js
@@ -9,7 +9,7 @@ const UmamiScript = () => {
         async
         defer
         data-website-id={siteMetadata.analytics.umamiWebsiteId}
-        src="https://umami.monimiller.com/umami.js" // Replace with your umami instance
+        src="https://tourmaline-dieffenbachia-bffe57.netlify.app/umami.js" // Replace with your umami instance
       />
     </>
   )
