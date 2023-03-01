7dd28974beb07459cbb024d9c72a26e4db4e00e2
Edmund Miller <edmund.a.miller@protonmail.com>
2023-02-28 20:42:14 -0600

    Fix nested posts getting pulled in

--- config/collections/index.js
+++ config/collections/index.js
@@ -1,6 +1,6 @@
 /** Returns all blog posts as a collection. */
 const getAllPosts = collection => {
-  const projects = collection.getFilteredByGlob('./src/posts/*.md');
+  const projects = collection.getFilteredByGlob('./src/posts/**/*.md');
   return projects.reverse();
 };
 
