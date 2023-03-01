be820623b94128ade7319097999ccd60182228cf
Edmund Miller <edmund.a.miller@protonmail.com>
2023-02-28 20:17:44 -0600

    Re-init with eleventy-excellent
    https://github.com/madrilene/eleventy-excellent

--- data/headerNavLinks.js
+++
@@ -1,8 +0,0 @@
-const headerNavLinks = [
-  { href: '/about', title: 'About' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/tags', title: 'Tags' },
-]
-
-export default headerNavLinks
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,159 +1,126 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-const colors = require('tailwindcss/colors')
+/* © Andy Bell - https://buildexcellentwebsit.es/ */
+
+const plugin = require('tailwindcss/plugin');
+const postcss = require('postcss');
+const postcssJs = require('postcss-js');
+
+const clampGenerator = require('./src/assets/css-utils/clamp-generator.js');
+const tokensToTailwind = require('./src/assets/css-utils/tokens-to-tailwind.js');
+
+// Raw design tokens
+const colorTokens = require('./src/assets/design-tokens/colors.json');
+const fontTokens = require('./src/assets/design-tokens/fonts.json');
+const spacingTokens = require('./src/assets/design-tokens/spacing.json');
+const textSizeTokens = require('./src/assets/design-tokens/text-sizes.json');
+
+// Process design tokens
+const colors = tokensToTailwind(colorTokens.items);
+const fontFamily = tokensToTailwind(fontTokens.items);
+const fontSize = tokensToTailwind(clampGenerator(textSizeTokens.items));
+const spacing = tokensToTailwind(clampGenerator(spacingTokens.items));
 
 module.exports = {
-  content: ['./pages/**/*.js', './components/**/*.js', './layouts/**/*.js', './lib/**/*.js'],
-  darkMode: 'class',
+  content: ['./src/**/*.{html,js,jsx,mdx,njk,twig,vue}'],
+  presets: [],
   theme: {
-    extend: {
-      spacing: {
-        '9/16': '56.25%',
-      },
-      lineHeight: {
-        11: '2.75rem',
-        12: '3rem',
-        13: '3.25rem',
-        14: '3.5rem',
-      },
-      fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        primary: colors.slate,
-        gray: colors.neutral,
-      },
-      typography: (theme) => ({
-        DEFAULT: {
-          css: {
-            color: theme('colors.gray.700'),
-            a: {
-              color: theme('colors.primary.500'),
-              '&:hover': {
-                color: `${theme('colors.primary.600')} !important`,
-              },
-              code: { color: theme('colors.primary.400') },
-            },
-            h1: {
-              fontWeight: '700',
-              letterSpacing: theme('letterSpacing.tight'),
-              color: theme('colors.gray.900'),
-            },
-            h2: {
-              fontWeight: '700',
-              letterSpacing: theme('letterSpacing.tight'),
-              color: theme('colors.gray.900'),
-            },
-            h3: {
-              fontWeight: '600',
-              color: theme('colors.gray.900'),
-            },
-            'h4,h5,h6': {
-              color: theme('colors.gray.900'),
-            },
-            pre: {
-              backgroundColor: theme('colors.gray.800'),
-            },
-            code: {
-              color: theme('colors.pink.500'),
-              backgroundColor: theme('colors.gray.100'),
-              paddingLeft: '4px',
-              paddingRight: '4px',
-              paddingTop: '2px',
-              paddingBottom: '2px',
-              borderRadius: '0.25rem',
-            },
-            'code::before': {
-              content: 'none',
-            },
-            'code::after': {
-              content: 'none',
-            },
-            details: {
-              backgroundColor: theme('colors.gray.100'),
-              paddingLeft: '4px',
-              paddingRight: '4px',
-              paddingTop: '2px',
-              paddingBottom: '2px',
-              borderRadius: '0.25rem',
-            },
-            hr: { borderColor: theme('colors.gray.200') },
-            'ol li::marker': {
-              fontWeight: '600',
-              color: theme('colors.gray.500'),
-            },
-            'ul li::marker': {
-              backgroundColor: theme('colors.gray.500'),
-            },
-            strong: { color: theme('colors.gray.600') },
-            blockquote: {
-              color: theme('colors.gray.900'),
-              borderLeftColor: theme('colors.gray.200'),
-            },
-          },
-        },
-        dark: {
-          css: {
-            color: theme('colors.gray.300'),
-            a: {
-              color: theme('colors.primary.500'),
-              '&:hover': {
-                color: `${theme('colors.primary.400')} !important`,
-              },
-              code: { color: theme('colors.primary.400') },
-            },
-            h1: {
-              fontWeight: '700',
-              letterSpacing: theme('letterSpacing.tight'),
-              color: theme('colors.gray.100'),
-            },
-            h2: {
-              fontWeight: '700',
-              letterSpacing: theme('letterSpacing.tight'),
-              color: theme('colors.gray.100'),
-            },
-            h3: {
-              fontWeight: '600',
-              color: theme('colors.gray.100'),
-            },
-            'h4,h5,h6': {
-              color: theme('colors.gray.100'),
-            },
-            pre: {
-              backgroundColor: theme('colors.gray.800'),
-            },
-            code: {
-              backgroundColor: theme('colors.gray.800'),
-            },
-            details: {
-              backgroundColor: theme('colors.gray.800'),
-            },
-            hr: { borderColor: theme('colors.gray.700') },
-            'ol li::marker': {
-              fontWeight: '600',
-              color: theme('colors.gray.400'),
-            },
-            'ul li::marker': {
-              backgroundColor: theme('colors.gray.400'),
-            },
-            strong: { color: theme('colors.gray.100') },
-            thead: {
-              th: {
-                color: theme('colors.gray.100'),
-              },
-            },
-            tbody: {
-              tr: {
-                borderBottomColor: theme('colors.gray.700'),
-              },
-            },
-            blockquote: {
-              color: theme('colors.gray.100'),
-              borderLeftColor: theme('colors.gray.700'),
-            },
-          },
-        },
-      }),
+    screens: {
+      md: '50em',
+      lg: '80em'
     },
+    colors,
+    spacing,
+    fontSize,
+    fontFamily,
+    fontWeight: {
+      normal: 400,
+      bold: 700,
+      black: 800
+    },
+    backgroundColor: ({theme}) => theme('colors'),
+    textColor: ({theme}) => theme('colors'),
+    margin: ({theme}) => ({
+      auto: 'auto',
+      ...theme('spacing')
+    }),
+    padding: ({theme}) => theme('spacing')
   },
-  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
-}
+  variantOrder: [
+    'first',
+    'last',
+    'odd',
+    'even',
+    'visited',
+    'checked',
+    'empty',
+    'read-only',
+    'group-hover',
+    'group-focus',
+    'focus-within',
+    'hover',
+    'focus',
+    'focus-visible',
+    'active',
+    'disabled'
+  ],
+
+  // Disables Tailwind's reset etc
+  corePlugins: {
+    preflight: false
+  },
+  plugins: [
+    // Generates custom property values from tailwind config
+    plugin(function ({addComponents, config}) {
+      let result = '';
+
+      const currentConfig = config();
+
+      const groups = [
+        {key: 'colors', prefix: 'color'},
+        {key: 'spacing', prefix: 'space'},
+        {key: 'fontSize', prefix: 'size'},
+        {key: 'fontFamily', prefix: 'font'}
+      ];
+
+      groups.forEach(({key, prefix}) => {
+        const group = currentConfig.theme[key];
+
+        if (!group) {
+          return;
+        }
+
+        Object.keys(group).forEach(key => {
+          result += `--${prefix}-${key}: ${group[key]};`;
+        });
+      });
+
+      addComponents({
+        ':root': postcssJs.objectify(postcss.parse(result))
+      });
+    }),
+
+    // Generates custom utility classes
+    plugin(function ({addUtilities, config}) {
+      const currentConfig = config();
+      const customUtilities = [
+        {key: 'spacing', prefix: 'flow-space', property: '--flow-space'},
+        {key: 'colors', prefix: 'spot-color', property: '--spot-color'}
+      ];
+
+      customUtilities.forEach(({key, prefix, property}) => {
+        const group = currentConfig.theme[key];
+
+        if (!group) {
+          return;
+        }
+
+        Object.keys(group).forEach(key => {
+          addUtilities({
+            [`.${prefix}-${key}`]: postcssJs.objectify(
+              postcss.parse(`${property}: ${group[key]}`)
+            )
+          });
+        });
+      });
+    })
+  ]
+};
