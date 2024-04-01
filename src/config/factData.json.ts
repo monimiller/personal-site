e538b7aefa3fd62be61eeddeb1656ee099bf4afe
Edmund Miller <git@edmundmiller.dev>
2024-04-01 15:05:19 -0500

    feat: Add Fast Facts section

---
+++ src/config/factData.json.ts
@@ -0,0 +1,22 @@
+export interface funFact {
+  // TODO image: ImageMetadata; // an imported image
+  fact: string;
+}
+
+
+export const factData: funFact[] = [
+  {
+    content: `I have a cockapoo named Phoebe, a husband named Edmund, and the three of us all share a love of food, walks, and spending time with our family and friends.`
+  },
+  {
+    content: `I played soccer in college and actually hold a conference record and a couple of school records.`
+  },
+  {
+    content: `I am deaf in my left ear and for a select period of time I wore a hearing aid to grade school.`
+  },
+  {
+    content: `I have the same middle name as my mom because she wanted me to have names related to both of my parents (Middle/Mom, Last/Dad).`
+  },
+];
+
+export default factData;
