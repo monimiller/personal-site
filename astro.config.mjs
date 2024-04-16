f18171e9825b8ebee9b7c3105436923f310fc07c
Edmund Miller <git@edmundmiller.dev>
2024-04-15 23:01:55 -0500

    build: Remove netlify and keystatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,20 +5,11 @@ import mdx from "@astrojs/mdx";
 import compress from "astro-compress";
 import AutoImport from "astro-auto-import";
 import react from "@astrojs/react";
-import keystatic from "@keystatic/astro";
-import netlify from "@astrojs/netlify";
 import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://monimiller.com",
-    output: "hybrid",
-    adapter: netlify({
-        imageCDN: false,
-    }),
-    redirects: {
-        "/admin": "/keystatic",
-    },
     markdown: {
         shikiConfig: {
             // Shiki Themes: https://github.com/shikijs/shiki/blob/main/docs/themes.md
@@ -27,6 +18,7 @@ export default defineConfig({
         },
     },
     // trailingSlash: "always",
+    prefetch: true,
     integrations: [
         // example auto import component into mdx files
         AutoImport({
@@ -37,7 +29,6 @@ export default defineConfig({
         }),
         mdx(),
         react(),
-        keystatic(),
         tailwind(),
         sitemap(),
         compress(),
