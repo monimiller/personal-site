e270f5ca632f34d58c8d6593ae3196cab6f46653
Monica Miller <33696269+monimiller@users.noreply.github.com>
2025-01-13 22:02:53 -0600

    update bio information

--- src/config/speakerData.json.ts
+++ src/config/speakerData.json.ts
@@ -7,14 +7,7 @@ export interface SpeakerItem {
 export const speakerData: SpeakerItem[] = [
   {
     question: "Short Bio",
-    answer: `Monica is a former data engineer turned developer advocate, who now works to improve the lives of other data engineers by creating informational resources, speaking at conferences, and writing about her experiences in the data space.  As a data engineer, she spent her time primarily developing and supporting data pipelines for both near-real time analytics and batch processing.`,
-  },
-  {
-    question: "Long Bio",
-    answer: `Monica Miller is a data engineer, conference speaker, and writer, currently working Developer Relations at Starburst Data. She is a Senior Developer Advocate and member of the Trino community.
-<br>
-<br>
-She graduated from The University of Texas at Dallas with a BS in mechanical engineering and received her MS from the The University of Texas at Dallas in Systems Engineering. When she's not speaking, writing, or mentoring, Moni likes to chase squirrels with her dog, Phoebe. She also played soccer in a past life.`,
+    answer: `Monica is a former data engineer and developer advocate turned product manager, who now focuses her time working alongside the Trino community. As a data engineer, she spent her time primarily developing and supporting data pipelines for both near-real time analytics and batch processing. As a developer advocate, she spent her time speaking at conferences, writing about her experiences in the data space, and creating informational resources.`
   },
   {
     question: "Headshot",
